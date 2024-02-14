9d4e86d410bd147f55786b6ec35fb3b30afee5d2
Ansel <anselanselmi@gmail.com>
2024-02-13 22:00:10 -0600

    Salience has been stripped from AgentForge

---
+++ CustomAgents/SummarizationAgent.py
@@ -0,0 +1,53 @@
+from agentforge.agent import Agent
+
+
+class SummarizationAgent(Agent):
+
+    def run(self, text=None, query=None):
+        try:
+            if query:
+                return self.run_query(query)
+            else:
+                return self.summarize(text)
+        except Exception as e:
+            self.logger.log(f"Error Running Summarization Agent: {e}", 'error')
+            return None
+
+    def run_query(self, query):
+        try:
+            text = self.get_search_results(query)
+            if text:
+                return self.summarize(text)
+        except Exception as e:
+            self.logger.log(f"Error running query: {e}", 'error')
+            return None
+
+    def get_search_results(self, query):
+        try:
+            params = {'collection_name': "Results", 'query': query}
+            search_results = self.storage.query_memory(params, 5).get('documents')
+
+            text = None
+            if search_results:
+                text = "\n".join(search_results[0])
+
+            return text
+        except Exception as e:
+            self.logger.log(f"Error fetching search results: {e}", 'error')
+            return None
+
+    def summarize(self, text):
+        try:
+            # Simply summarize the given text
+            return super().run(text=text)
+        except Exception as e:
+            self.logger.log(f"Error summarizing text: {e}", 'error')
+            return None
+
+    def build_output(self):
+        try:
+            parsed_yaml = self.functions.agent_utils.parse_yaml_string(self.result)
+            self.output = parsed_yaml.get("summary", "").lower().strip()
+        except Exception as e:
+            self.logger.parsing_error(self.result, e)
+            self.output = self.result
